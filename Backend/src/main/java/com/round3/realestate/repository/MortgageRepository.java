6c64549813e01f5ef187f14c309ebdbdc9d622dd
Arielparicio28 <arielaparicio100@gmail.com>
2025-04-07 11:21:12 +0200

    Create Mortgage Repository

--- Backend/src/main/java/com/round3/realestate/repository/MortgageRepository.java
+++ Backend/src/main/java/com/round3/realestate/repository/MortgageRepository.java
@@ -1,9 +1,9 @@
 package com.round3.realestate.repository;
 
-import com.round3.realestate.entity.Property;
+import com.round3.realestate.entity.Mortgage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PropertyRepository extends JpaRepository<Property,Long> {
+public interface MortgageRepository extends JpaRepository<Mortgage,Long> {
 }
